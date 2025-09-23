ead4c6a7fba4dae43e54b12607bb39c3c71d5943
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-09-23 11:47:11 -0300

    add: update invoice func to invoice page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,7 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+/* CREATE INVOICE **/
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoices(formData: FormData) {
@@ -32,5 +33,27 @@ export async function createInvoices(formData: FormData) {
   `;
 
   revalidatePath('/dashboard/invoices'); // Clear's Next.js's cache so fresh data can be shown
+  redirect('/dashboard/invoices');
+}
+
+/* UPDATE INVOICE **/
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
